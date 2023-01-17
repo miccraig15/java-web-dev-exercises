3695295f17a2b3de60e98c007af51f111b4c3324
miccraig15 <miccraig15@gmail.com>
2023-01-17 16:49:52 -0600

    Completed Chapter 2.5 Studio Area of a Circle

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the radius of the circle: ");
+        double radius = input.nextDouble();
+        double area = Circle.getArea(radius);
+        System.out.println("The area of a circle with radius " + radius + " is: " + area );
+    }
+}
